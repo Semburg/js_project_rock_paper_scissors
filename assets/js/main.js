ad49f21437205677ef8c20ed6fe33173703316a7
Semburg <semburg.sb@gmail.com>
2021-02-26 16:01:46 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,26 @@
-
-console.log("works")
-
 const counterIsThree = document.getElementById("three")
 const counterIsFive = document.getElementById("five")
 const counterIsNine = document.getElementById("nine")
 const counterIs12 = document.getElementById("twelve")
-
 const triesTotal = document.getElementById("tries_total")
 const counterTries = document.getElementById("counter_tries")
-
 const yoursWins = document.getElementById("yours_wins")
 const compsWins = document.getElementById("comps_wins")
+const gameIn = document.getElementById("game_in");
+const compsChoose = document.getElementById("comps_choose")
+const newGame = document.getElementById("new_game");
+const flowResults = document.getElementById("flow_results");
+const handsArray = ["ROCK", "PAPER", "SCISSORS"]
+
 let yoursWinsNum = 0;
 let compsWinsNum = 0;
-
-
-
 let tryCount=3;
+let counter = 1;
+
 counterIsThree.addEventListener("click", ()=> tryCount = 3)
 counterIsFive.addEventListener("click", ()=> tryCount = 5)
 counterIsNine.addEventListener("click", ()=> tryCount = 9)
 counterIs12.addEventListener("click", ()=> tryCount = 12)
-console.log(tryCount)
-
-
-
 
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
@@ -32,42 +28,9 @@ const scissors = document.getElementById("scissors");
 
 let showInformations = document.getElementById("show_nformations")
 
-// const tryCount = document.getElementsByName("try_count")
-
-
 rock.addEventListener("click", choosedHand)
 paper.addEventListener("click", choosedHand)
 scissors.addEventListener("click", choosedHand)
-
-// rock.addEventListener("click", fadeOutEffect)
-
-
-
-const compsChoose = document.getElementById("comps_choose")
-
-const newGame = document.getElementById("new_game");
-const flowResults = document.getElementById("flow_results");
-
-
-const handsArray = ["ROCK", "PAPER", "SCISSORS"]
-
-let counter = 1;
-
-
-function fadeOutEffect() {
-
-    let fadeEffect = setInterval(function (){
-        compsChoose.style.opacity = 0.5;
-        if(!compsChoose.style.opacity){
-            compsChoose.style.opacity = 1;
-        }else if(compsChoose.style.opacity > 0){
-            compsChoose.style.opacity -= 0.1;
-        }else {
-            clearInterval(fadeEffect);
-        }
-    },200);
-}
-
 
 function choosedHand() {
     let compsHand = Math.floor(Math.random() * 3)
@@ -88,7 +51,6 @@ function choosedHand() {
     if( (compsHand === 0 && myHand === "PAPER") ||
         (compsHand === 1 && myHand === "SCISSORS") ||
         (compsHand === 2 && myHand === "ROCK")){
-        console.log("You win")
         yoursWinsNum++;
         yoursWins.innerHTML = yoursWinsNum;
         showInformations.innerHTML =`Your ${myHand} beats Comp's ${handsArray[compsHand]}`
@@ -97,19 +59,16 @@ function choosedHand() {
 
 
     } else if(handsArray[compsHand] === myHand){
-        console.log("DRAW")
         showInformations.innerHTML =`Your ${myHand} vs Comp's ${handsArray[compsHand]} - DRAW`
         showInformations.style.color = "#666"
 
     } else{
-        console.log("Comps win")
         compsWinsNum++;
         compsWins.innerHTML = compsWinsNum;
         showInformations.innerHTML =`Your ${myHand} loses to Comp's ${handsArray[compsHand]} `
         showInformations.style.color = "red"
     }
     if( Number(tryCount/yoursWinsNum)<2 || Number(tryCount/compsWinsNum)<2 ){
-        console.log("game over")
         newGame.style.display="inline-block";
         if(yoursWinsNum>compsWinsNum){
             flowResults.innerHTML = "you win";
@@ -123,6 +82,3 @@ function choosedHand() {
     counter++
 }
 
-
-const gameIn = document.getElementById("game_in");
-// newGame.style.display = "none"
